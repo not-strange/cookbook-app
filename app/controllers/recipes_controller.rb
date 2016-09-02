c81fa3429e82db0f2d3e04abd9c0dbb331511765
JackMarx <seriousfools@gmail.com>
2016-09-01 20:14:43 -0500

    new/create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,27 @@
 class RecipesController < ApplicationController
-  def one
-    @recipe = Recipe.find(1)
-  end
-
-  def all
+  def index
     @recipes = Recipe.all
   end
+
+  def new
+    
+  end
+
+  def create
+    @recipe = Recipe.create(title: params[:title],
+                            chef: params[:chef],
+                            prep_time: params[:prep_time],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions])
+
+    render 'show.html.erb'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/one' => 'recipes#one'
-  get '/all' => 'recipes#all'
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+  
+  get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show'
 end
