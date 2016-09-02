fbad7c40e438c97f76b7d4cf240ce86c0870b166
JackMarx <seriousfools@gmail.com>
2016-09-01 20:58:18 -0500

    edit/update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,21 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(title: params[:title],
+                   prep_time: params[:prep_time],
+                   chef: params[:chef],
+                   ingredients: params[:ingredients],
+                   directions: params[:directions])
+
+    render 'show.html.erb'
+  end
 end
 
 
