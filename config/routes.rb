aca20d054931f0aadf12db7670740fd63b855c95
JackMarx <seriousfools@gmail.com>
2016-09-13 20:43:50 -0500

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
 
 
